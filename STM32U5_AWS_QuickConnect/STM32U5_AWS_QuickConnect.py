03c468f60fe1efff7ddb58594960d3c74661a933
SlimJallouli <slim.jallouli@st.com>
2022-09-05 09:59:52 -0700

    pass bin file path to flash.py. Print version number

--- STM32U5_AWS_QuickConnect/STM32U5_AWS_QuickConnect.py
+++ STM32U5_AWS_QuickConnect/STM32U5_AWS_QuickConnect.py
@@ -10,10 +10,13 @@ SSID = 'ssid'
 PSWD = 'pswd'
 DUMMY_SSID = '0'
 DUMMY_PSWD = '0'
+BIN_FILE = '..\\Projects\\b_u585i_iot02a_ntz\\Debug\\b_u585i_iot02a_ntz.bin'
+VERSION="1.0.0"
 
 HELP = ['openDashboard.py options:', 
         '\n\t-h or --help for help',
         '\n\t-i for interactive mode',
+        '\n\t--version for file version',
         '\n\t--ssid=[WiFi SSID]', 
         '\n\t--password=[WiFi Password]']
 
@@ -48,7 +51,7 @@ def getHiddenParam(curParam, label):
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv,"hi", ["help", "interactive", "ssid=", "password=", "key=", "secret-key="])
+        opts, args = getopt.getopt(argv,"hi", ["help", "interactive", "version", "ssid=", "password=", "key=", "secret-key="])
     except getopt.GetoptError:
         print("Parameter Error")
         sys.exit(1)
@@ -63,6 +66,10 @@ def main(argv):
         if opt in ("-h", "--help"):
             print(*HELP)
             sys.exit(1)
+        
+        elif opt in ("--version"):
+            print("STM32U5_AWS_QuickConnect.py version: " +VERSION)
+            sys.exit(1)
 
         elif opt in ("--ssid"):
             ssid  = arg
@@ -74,18 +81,15 @@ def main(argv):
             interactiveMode = True
 
 
-
     if interactiveMode:
         ssid = getParam(ssid, "Wi-Fi SSID")
         pswd = getHiddenParam(pswd, "Wi-Fi Password")
 
     
-    cmd(['python', 'utils\\flash.py'])
+    cmd(['python', 'utils\\flash.py', '--bin-file='+BIN_FILE])
     cmd(['python', 'utils\\setWiFiParam.py', '--ssid=' + DUMMY_SSID, '--password='+ DUMMY_PSWD])
     cmd(['python', 'utils\\provision.py', '--thing-name=' + name, '--wifi-ssid=' +  ssid, '--wifi-credential=' + pswd])
     cmd(['python', 'utils\\readSerial.py'])
-
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
